891125046e576e10e2452b570b181ce9b5fd8ff0
TobiasSchejtman <tobias.schejtman@post.runi.ac.il>
2024-12-04 18:46:43 +0200

    exercise 2 finished

--- Primes.java
+++ Primes.java
@@ -1,5 +1,53 @@
 public class Primes {
+
     public static void main(String[] args) {
-        // Replace this statement with your code
+       
+        int n = Integer.parseInt(args[0]);
+        boolean[] isPrime = sieveOfEratosthenes(n);
+
+        System.out.println("Prime numbers up to " + n + ":");
+        
+        int primeCount = 0;
+        int j = 2; 
+
+        while (j <= n) {
+            if (isPrime[j]) {
+                System.out.println(j);
+                primeCount++;
+            }
+            j++;
+        }
+
+        
+        double percentage = (primeCount * 100) / n;
+        System.out.println("There are " + primeCount + " primes between 2 and " + n + " (" + (int) percentage + "% are primes)");
+    }
+
+    public static boolean[] sieveOfEratosthenes(int n) {
+        
+        boolean[] isPrime = new boolean[n + 1];
+
+        int i = 2;
+        while (i <= n) {
+            isPrime[i] = true;
+            i++;
+        }
+
+        int p = 2;
+        while (p * p <= n) {
+            if (isPrime[p]) {
+                int multiple = p * p;
+
+                while (multiple <= n) {
+                    isPrime[multiple] = false;
+                    multiple += p;
+                }
+            }
+            p++;
+        }
+
+        return isPrime;
     }
 }
+
+   
